5b6cc6253406c6194017f338871f18a157a3d538
aibol_vm_netman <aibol.kemeldinov@colorado.edu>
2025-02-08 17:00:15 -0700

    all

--- sshInfo.py
+++ sshInfo.py
@@ -1,7 +1,32 @@
 #/usr/bin/env python3
 import csv
+import os
 
-with open('sshInfo.csv', mode='r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        print(row)
+file_path = 'sshInfo.csv'
+
+if os.path.exists(file_path):
+    try:
+        with open('sshInfo.csv', mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            print(f"{file_path} is exists")
+            rows=list(reader)
+            if not rows:
+                print(f"{file_path} is empty")
+
+            else:
+                for row in rows:
+                    print(row)
+
+    except csv.Error as e:
+                print(f"Cannot read {file_path}")
+
+else:
+    print(f"{file_path} is not exists")
+
+
+
+#
+#with open('sshInfo.csv', mode='r', encoding='utf-8') as file:
+#    reader = csv.DictReader(file)
+#    for row in reader:
+#        print(row)
---
+++ ssh_check.py
@@ -0,0 +1,10 @@
+import subprocess
+import csv
+
+for csv 
+
+ans = subprocess.call(["ping", {host}])
+if ans == 0:
+    print("Command executed.")
+else:
+    print("Command failed.", return_code)
