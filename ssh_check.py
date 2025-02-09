86c3fee592ff5c42eb7875f6de657a5376d3b765
aibolkz <aibol.kemeldinov@colorado.edu>
2025-02-08 21:51:47 -0700

    Update ssh_check.py
    123

--- ssh_check.py
+++ ssh_check.py
@@ -2,6 +2,9 @@ import subprocess
 import csv
 
 with open('sshInfo.csv', mode='r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        ip = reader['host']
 
 ans = subprocess.call(["ping", {host}])
 if ans == 0:
