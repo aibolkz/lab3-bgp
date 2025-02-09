fe6ec7fe0f3397fb031fba4b7062e480245e7604
aibolkz <aibol.kemeldinov@colorado.edu>
2025-02-08 21:59:27 -0700

    Update ssh_check.py
    123

--- ssh_check.py
+++ ssh_check.py
@@ -5,9 +5,9 @@ with open('sshInfo.csv', mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
         ip = row['host']
-        command = ["ping", "-c", "-2", ip]
+        command = ["ping", "-c", "2", ip]
         ans = subprocess.call(command)
         if ans == 0:
             print("ping was succesful.")
         else:
-            print("Ping failed, command is {ip} with return code {ans})
+            print(f"Ping failed, command is {ip} with return code {ans})
